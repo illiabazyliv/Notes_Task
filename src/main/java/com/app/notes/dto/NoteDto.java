dac705d089e8191ddcb9d418731a2dcece1d4163
Chiaok <bazylivillya@gmail.com>
2025-09-17 15:01:45 +0300

    changed dto

--- src/main/java/com/app/notes/dto/NoteDto.java
+++ src/main/java/com/app/notes/dto/NoteDto.java
@@ -19,6 +19,17 @@ public class NoteDto {
     public NoteDto() {
     }
 
+    public NoteDto(Long id, String title, String content,
+                   LocalDateTime createdAt, Long authorId,
+                   String authorName) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.authorId = authorId;
+        this.authorName = authorName;
+    }
+
     public Long getId() {
         return id;
     }
